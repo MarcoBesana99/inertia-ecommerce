4cf410e316c552e37a3f7f2d420a89af84cf0ba7
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-06-17 14:27:16 +0200

    vue navbar

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,18 +1,6 @@
 <template>
-  <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      dark:bg-gray-900
-      sm:items-center
-      sm:pt-0
-    "
-  >
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+  <div>
+    <div>
       <!-- Page Content -->
       <main>
         <slot></slot>
